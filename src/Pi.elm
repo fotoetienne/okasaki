23365cac4da709756da3d3c4073a9e05a30ae97a
Stephen <github.erty@dfgh.net>
2016-09-12 23:15:34 -0400

    Correct pi formula

--- src/Pi.elm
+++ src/Pi.elm
@@ -8,7 +8,7 @@ import Color exposing (Color, red, grey, black)
 import String
 import Window
 import Time exposing (fps, inMilliseconds)
-import Text exposing (fromString)
+import Text exposing (fromString, typeface, height)
 
 type alias Point = { x:Float, y:Float }
 
@@ -21,7 +21,7 @@ euclidean {x,y} = sqrt (x^2 + y^2)
 
 upstate : Point -> State -> State
 upstate pt ((hitCount, hitList), (missCount, missList)) =
-    if euclidean pt < 1 then
+    if euclidean pt <= 1 then
         ((hitCount + 1, pt :: hitList), (missCount, missList))
     else
         ((hitCount, hitList), (missCount + 1, pt :: missList))
@@ -33,10 +33,10 @@ scale w x = x * (toFloat w)
 
 greyRect w = C.filled grey <| C.rect (scale w 2) (scale w 1)
 
-dot color = C.filled color (C.circle 1)
+dot color = C.filled color (C.circle 3)
 
 estimatePi : Int -> Int -> Float
-estimatePi hits misses = (toFloat hits) / (toFloat misses)
+estimatePi hits misses = 4 * (toFloat hits) / (toFloat (hits + misses))
 
 renderPoint : Int -> Color -> Point -> Form
 renderPoint w color {x,y} = move (scale w x, scale w (y - 0.5)) (dot color)
@@ -44,10 +44,11 @@ renderPoint w color {x,y} = move (scale w x, scale w (y - 0.5)) (dot color)
 view : (Int,Int) -> State -> Element
 view (w,h) ((hitCount, hits), (missCount, misses)) =
     let s = min (w // 2) h in
-    collage w h <| greyRect s ::
-      C.text (fromString <| String.left 7 <| toString <| estimatePi hitCount missCount) ::
+    collage w h <| -- greyRect s ::
       (List.map (renderPoint s red) hits) ++
-      (List.map (renderPoint s black) misses)
+      (List.map (renderPoint s black) misses) ++
+      [C.text (height 72 <| typeface ["helvetica"] <| fromString <|
+         String.left 7 <| toString <| estimatePi hitCount missCount)]
 
 genPoint : Random.Seed -> (Point, Random.Seed)
 genPoint s0 =
@@ -63,7 +64,7 @@ genPoints n s0 =
 
 signalPointSeed : Signal (List Point, Random.Seed)
 signalPointSeed =
-    foldp (\_ (_,s) -> genPoints 100 s) (42 |> initialSeed |> (genPoints 1)) (fps 15)
+    foldp (\_ (_,s) -> genPoints 10 s) (42 |> initialSeed |> (genPoints 1)) (fps 25)
 
 signalPoint : Signal (List Point)
 signalPoint = Signal.map fst signalPointSeed
